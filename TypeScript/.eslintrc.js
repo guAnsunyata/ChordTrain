1ef5b567fc7c8273a9fb7c1277cfd732b3613f5c
guansunyata <guansunyata@gmail.com>
2021-02-02 22:45:20 +0800

    Setup eslint & prettier for incoming refactoring

---
+++ TypeScript/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['prettier', '@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        tabWidth: 2,
+        arrowParens: 'always',
+        singleQuote: true,
+        trailingComma: 'es5',
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    ],
+  },
+};
