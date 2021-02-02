a7035d52f51337db16997f2f9f8ea728de55d44e
guansunyata <guansunyata@gmail.com>
2021-02-02 22:47:42 +0800

    Refactoring #2 eager map (cont. #1)

---
+++ TypeScript/src/2-eager-map/main.ts
@@ -0,0 +1,32 @@
+import { EagerClassifier } from './classifier'
+
+export default function main() {
+  const classifier = new EagerClassifier()
+
+  const imagine = ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7']
+  const somewhereOverTheRainbow = ['c', 'em', 'f', 'g', 'am']
+  const tooManyCooks = ['c', 'g', 'f']
+  const iWillFollowYouIntoTheDark = ['f', 'dm', 'bb', 'c', 'a', 'bbm']
+  const babyOneMoreTime = ['cm', 'g', 'bb', 'eb', 'fm', 'ab']
+  const creep = ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6']
+  // prettier-ignore
+  const paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'];
+  // prettier-ignore
+  const toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'];
+  const bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#']
+
+  classifier.addRecord(imagine, 'easy')
+  classifier.addRecord(somewhereOverTheRainbow, 'easy')
+  classifier.addRecord(tooManyCooks, 'easy')
+  classifier.addRecord(iWillFollowYouIntoTheDark, 'medium')
+  classifier.addRecord(babyOneMoreTime, 'medium')
+  classifier.addRecord(creep, 'medium')
+  classifier.addRecord(paperBag, 'hard')
+  classifier.addRecord(toxic, 'hard')
+  classifier.addRecord(bulletproof, 'hard')
+
+  classifier.setup()
+  console.log(classifier.classify(['d', 'g', 'e', 'dm']))
+  // prettier-ignore
+  console.log(classifier.classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']))
+}
